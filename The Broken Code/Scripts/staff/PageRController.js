fe36b9688e00a6c944793043dab7fc958a04dfd7
Jesse Montano <jmontano@sangerisd.net>
2015-11-03 02:07:53 -0600

    Got the Help page and the Refill page to talk with the database.

--- The Broken Code/Scripts/staff/PageRController.js
+++ The Broken Code/Scripts/staff/PageRController.js
@@ -1,42 +1,20 @@
-restaurantApp.controller('PageRController',
-     ['$rootScope', '$scope', '$http', 'ParseService', '$location', '$q',
-     function ($rootScope, $scope, $http, $location, ParseService, $q) {
-         var RefillName = [];
-         var RefillDfd = $q.defer();
-         var Refill = Parse.Object.extend("Refill");
-         var queryRefill = new Parse.Query(Refill);
-         var value = element(by.binding('example.value'));
-         var input = element(by.model('example.value'));
-         queryRefill.find({
-             success: function (data) {
-                 angular.forEach(data, function (result) {
-                     RefillName.push({
-                         ID: result.get("RefillID"),
-                         TableID: result.get("TableID"),
-                         DrinkName: result.get("DrinkName"),
-                     });
-                     
-                 });
-             },
-             error: function (error) {
-                 alert("Error: " + error.code + " " + error.message);
-             }
-         }).then(function (data) {
-
-             RefillDfd.resolve(data);
-             
-         },
-         function (error) {
-             RefillDfd.reject(data);
-
-         });
-         RefillDfd.promise
-         .then(function (Refill) {
-            
-             $scope.Refill = RefillName;
-         })
-         .catch(function (error) {
-            
-         });
-         $scope.headingCaption = 'Refill';
-     }]);
+function PageRController() 
+{
+            var Refill = Parse.Object.extend("Refill");
+            var refill = new Refill();
+            var String = document.getElementById('refilltext').value;
+                                            
+            refill.set("RefillID", String);
+            refill.set("RefillID", String);
+            refill.set("RefillID", String);
+            refill.set("RefillID", String);
+            refill.set("RefillID", String);
+            refill.save(null, {
+                success: function (refill) {
+                    alert("Thank you! Your request has been sent!");
+                },
+                error: function (refill, error) {
+                   // alert("Failed to create!");
+                }
+            });
+}
