b6874c453fe146da01a3caa2dbed64b05fd91089
Jesse Montano <jmontano@sangerisd.net>
2015-11-02 23:37:22 -0600

    Help page backend stuff...

--- The Broken Code/Scripts/staff/helppage.js
+++ The Broken Code/Scripts/staff/helppage.js
@@ -1,40 +1,18 @@
-restaurantApp.controller('HelpController',
-     ['$rootScope', '$scope', '$http', 'ParseService', '$location', '$q',
-     function ($rootScope, $scope, $http, $location, ParseService, $q) {
-         var HelpName = [];
-         var HelpDfd = $q.defer();
-         var Help = Parse.Object.extend("Help");
-         var queryHelp = new Parse.Query(Help);
-         queryHelp.find({
-             success: function (data) {
-                 angular.forEach(data, function (result) {
-                     HelpName.push({
-                         Request: result.get("HelpRequest"),
-                         Status: result.get("HelpStatus"),
-                         TableID: result.get("TableID"),
-                     });
-                     
-                 });
-             },
-             error: function (error) {
-                 alert("Error: " + error.code + " " + error.message);
-             }
-         }).then(function (data) {
 
-             HelpDfd.resolve(data);
-             
-         },
-         function (error) {
-                HelpDfd.reject(data);
-
-         });
-         HelpDfd.promise
-         .then(function (Help) {
-            
-             $scope.Help = HelpName;
-         })
-         .catch(function (error) {
-            
-         });
-         $scope.headingCaption = 'Help';
-     }]);
+function HelpF() 
+{
+            var Help = Parse.Object.extend("Help");
+            var help = new Help();
+            var String = document.getElementById('helptext').value;
+            console.log("Hello;"+String);
+            help.set("HelpRequest", String);
+            help.save(null, {
+                success: function (help) {
+                    alert("Thank you! Your request has been sent!");
+                },
+                error: function (help, error) {
+                    //alert("Failed to create!");
+                }
+            });
+        }
+  
