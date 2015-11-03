2cacbb0031e48cf91af0f3833f0f019b38c64fb0
Juan Hernandez <juanhernandez12@my.unt.edu>
2015-11-03 00:09:09 -0600

    Assigning table to users

--- The Broken Code/Scripts/controller.js
+++ The Broken Code/Scripts/controller.js
@@ -3,8 +3,8 @@
 /* Controlers */
 
 restaurantApp.controller('MainController',
-     ['$rootScope', '$scope', '$http', '$location', '$window', 'ParseService',
-     function ($rootScope, $scope, $http, $location, $window, ParseService) {
+     ['$rootScope', '$scope', '$http', '$location', '$window', '$q', 'ParseService',
+     function ($rootScope, $scope, $http, $location, $window, $q, ParseService) {
 
           //Get the current user
           $rootScope.currentUser = Parse.User.current();
@@ -22,9 +22,7 @@ restaurantApp.controller('MainController',
           
           //Called when user clicks Register button
           $scope.register = function () {
-               console.log("hit_register");
                var user = new Parse.User();
-               console.log("hit_register");
                user.set("Name", $scope.newuser.name);
                user.set("username", $scope.newuser.username);
                user.set("password", $scope.newuser.password);
@@ -34,9 +32,10 @@ restaurantApp.controller('MainController',
                     success: function (user) {                        
                          // Welcome the user and close the modal window
                          $rootScope.currentUser = Parse.User.current();
-                         alert("Login Successful! Welcome " + user.username + "!");
+                         alert("Login Successful! Welcome " + user.getUsername() + "!");
                          $('#registerModal').modal('hide');
                          $window.location.reload()
+                         getTable(user);
                     },
                     error: function(user, error) {
                          // Show the error message somewhere and let the user try again.
@@ -48,9 +47,12 @@ restaurantApp.controller('MainController',
           //Welcome user and redirect them to the main page
           function loginSuccessful(user) {
                $rootScope.$apply(function () {
+                    //Set the currentUser, welcome the user, and get them a table
                     $rootScope.currentUser = Parse.User.current();
-                    alert("Login Successful! Welcome " + user.username + "!");
+                    alert("Login Successful! Welcome " + user.getUsername() + "!");
                     $location.path("/");
+                    getTable(user);
+
                });
           }
 
@@ -58,23 +60,39 @@ restaurantApp.controller('MainController',
           function loginUnsuccessful(user, error) {
                switch (error.code) {
                     case Parse.Error.INVALID_SESSION_TOKEN:
-                         console.log("hit invalid");
                          Parse.User.logOut();
                          break;
                     default:
                          alert("Error: " + error.message + " (" + error.code + ")");
                          break;
                }//end switch
-              //alert("Error: " + error.message + " (" + error.code + ")");
           }
 
           //Log out function
           $rootScope.logOut = function () {
-               console.log("hit logout");
                $rootScope.currentUser = null;
                Parse.User.logOut();
                $location.path("/");
           };
+
+          //Function to get the user a table
+          function getTable(user) {
+               var Table = Parse.Object.extend("Table");
+               var tableQuery = new Parse.Query(Table);
+               tableQuery.equalTo("Available", true);
+               //Gets the first free table
+               tableQuery.first({
+                    success: function (data) {
+                         $rootScope.table = data;
+                         $rootScope.table.set("Available", false);
+                         $rootScope.table.save();
+                         return $rootScope.table;
+                    },
+                    error: function (error) {
+                         alert("Error: " + error.code + " " + error.message);
+                    }
+               });// End tableQuery.first              
+          }
 
 }]);
 
@@ -82,6 +100,5 @@ restaurantApp.controller('MainController',
 /*
 
      EOF
-
 
 */
