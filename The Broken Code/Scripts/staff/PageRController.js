ff5c26bd6367831ca2230e762082010e624306e6
Garrett Addington <garrettaddington@my.unt.edu>
2015-11-04 12:09:03 -0600

    Jessie in the pageRcontrolleri wrote the rough outline how to get that to work.

--- The Broken Code/Scripts/staff/PageRController.js
+++ The Broken Code/Scripts/staff/PageRController.js
@@ -1,4 +1,13 @@
-function PageRController() 
+//Garrett Comment guide
+//First off, you can to set up the ID on the html page so they all unique. ie refillText1 and etc
+//Once that is done, what you will do is create a new variable call RefillList which is an array of drinks needed for refill
+//Now we need to create a loop to collect the refill amount for every drink if they do't need a refill denote that as 0
+//You do this by appending that number to the end of the array called RefillList or the like
+//Once you finish pulling all the data from the html side you will set it into the database then save the object
+//That should resolve this problem. You will need to create a another function on waitstaff side to break the array down and list out the drink by name.
+//Another method you can do is create a data structue directory if javascript doesn't have it, but we running low on time. 
+//If you need help i will be on the chat
+function PageRController()
 {
             var Refill = Parse.Object.extend("Refill");
             var refill = new Refill();
