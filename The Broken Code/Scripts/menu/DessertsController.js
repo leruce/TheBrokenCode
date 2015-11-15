224cc438febd704a8ae6972c88e7abcd5d64073a
Thomas McCullough <thomasmccullough742@gmail.com>
2015-11-14 22:12:24 -0600

    Revert "Added new attribute to order to keep track of additional information (whether the order has been claimed from the kitchen)"
    This reverts commit 9cebbfdb3f6bef81774ff3cbe43d8e8d50b349da.

--- The Broken Code/Scripts/menu/DessertsController.js
+++ The Broken Code/Scripts/menu/DessertsController.js
@@ -94,7 +94,6 @@ restaurantApp.controller('DessertMenuController',
               order.set("ItemsOrdered", [_foodObject.FoodID]);
               order.set("Cost", _foodObject.Price);
               order.set("Ordered", false);
-              order.set("Claimed", false);
               order.set("Completed", false);
               order.set("InProgress", false);
               order.set("Paid", false);
